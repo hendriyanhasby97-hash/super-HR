26033c67bf594c986d6defd45439cc5359ab2ad8
hendriyanhasby97-hash <hendriyanhasby.97@gmail.com>
2026-05-22 10:07:32 +0700

    Delete src/lib directory

--- src/lib/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Mengambil variabel dari environment
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-// Inisialisasi client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
